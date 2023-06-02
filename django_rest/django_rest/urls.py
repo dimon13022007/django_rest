0dc68717a469d0fb61def1fa76112cb0aae13bde
dimon13022007 <dimon13022007@gmail.com>
2023-06-02 21:29:28 +0300

    Initial commit

---
+++ django_rest/django_rest/urls.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path, include
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('freelance.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    path('api-auth/', include('rest_framework.urls')),
+
+]
---
+++ django_rest/freelance/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, redirect
+
+def chat2(request):
+    return render(request, 'main/chat.html')
+
+
+
+
+
