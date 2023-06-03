9ddc3c1f07e0da25c027a88633ebb45208bf1eff
dimon13022007 <dimon13022007@gmail.com>
2023-06-03 22:47:56 +0300

    c

--- django_rest/freelance/serialazers.py
+++ django_rest/freelance/serialazers.py
@@ -1,11 +1,30 @@
 from rest_framework import serializers
 from .models import Women
-
-
-class WomenSerialazers(serializers.ModelSerializer):
-    class Meta:
-        model = Women
-        fields = ('title', 'cat_id', 'content')
+from rest_framework.renderers import JSONRenderer
 
 
 
+# class WomenModel:
+#
+#     def __init__(self, title, content):
+#         self.title = title
+#         self.content = content
+
+
+class WomenSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+    time_create = serializers.DateTimeField()
+    time_update = serializers.DateTimeField()
+    is_publisher = serializers.BooleanField(default=True)
+    cat_id = serializers.IntegerField()
+
+
+
+
+# def encode():
+#     model = WomenModel('ddd','Content: asdad')
+#     model_sr = WomenSerializer(model)
+#     print(model_sr.data, type(model_sr.data), sep='\n')
+#     json = JSONRenderer().render(model_sr.data)
+#     print(json)
--- django_rest/freelance/views.py
+++ django_rest/freelance/views.py
@@ -1,36 +1,24 @@
 from django.shortcuts import render, redirect
 from rest_framework import generics
-from .models import Women
-from .serialazers import WomenSerialazers
-from rest_framework.views import APIView
 from rest_framework.response import Response
-def chat2(request):
-    return render(request, 'main/chat.html')
-
-
-from django.shortcuts import render, redirect
-from rest_framework import generics
+from rest_framework.permissions import DjangoModelPermissionsOrAnonReadOnly
 from .models import Women
-from .serialazers import WomenSerialazers
+from .serialazers import WomenSerializer
 from rest_framework.views import APIView
-from rest_framework.response import Response
 
 
 def chat2(request):
     return render(request, 'main/chat.html')
+
 
 
 class WomenApiView(APIView):
-    def get_queryset(self):
-        return Women.objects.all()
+    queryset = Women.objects.all()
 
     def get(self, request):
-        queryset = self.get_queryset()
-        serializer = WomenSerialazers(queryset, many=True)
-        return Response(serializer.data)
-
-
-
+        w = Women.objects.all()
+        serializer = WomenSerializer(w, many=True)
+        return Response({'posts': serializer.data})
 
 
 
